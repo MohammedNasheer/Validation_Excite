cecd3f33c28fd770de98fd10348d549e032376e5
Nasheerkhan <35324833+Nasheerkhan@users.noreply.github.com>
2018-06-07 11:26:26 +0530

    Add files via upload

---
+++ transaction.sol
@@ -0,0 +1,50 @@
+pragma solidity ^0.4.16;
+ 
+ 
+
+contract minister{
+ 
+
+ 
+ 
+    address sbibank;  
+    bool _switch=false;  
+     
+
+function minister1() public{     
+   
+     sbibank =msg.sender; 
+  
+  } 
+   modifier ifOwner(){ 
+ 
+      require(sbibank!=msg.sender);
+      
+  _; 
+ 
+   } 
+   
+   
+    function depositFunds() public payable{ 
+         
+  
+  } 
+    function withDrawFunds(uint amount) public ifOwner{ 
+   
+     if(sbibank.send(amount)){ 
+  
+          _switch=true; 
+   
+     } 
+        else { 
+      
+      _switch=false; 
+        } 
+    }
+ 
+    function getfunds()public constant ifOwner returns(uint){ 
+
+        return this.balance; 
+
+    } 
+} 
